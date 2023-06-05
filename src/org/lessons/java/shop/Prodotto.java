9bf022d85a67bdf122c2b46b7a5e9c9ada8d65ac
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-06-06 00:43:20 +0200

    add Java shop (no-bonus)

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,81 @@
+//Nel progetto java-oop-1, package org.lessons.java.shop,
+// creare la classe Prodotto che gestisce i prodotti dello shop.
+//Un prodotto è caratterizzato da:
+//- codice (numero intero)
+//- nome
+//- descrizione
+//- prezzo
+//- iva
+
+//Usate opportunamente i livelli di accesso (public, private),
+// i costruttori,
+// i metodi getter e setter ed eventuali altri metodi di “servizio” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+
+//Il prodotto esponga sia un metodo per avere il prezzo base
+// che uno per avere il prezzo comprensivo di iva
+
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+
+//Nello stesso package aggiungete una classe Main
+// con metodo main nella quale testate tutte le funzionalità della classe Prodotto
+// BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare
+// a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+// Buon lavoro!
+
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+    // FIELDS (private)
+    private int codice;
+    protected String nome;
+    protected String descrizione;
+    protected double prezzo;
+    protected double iva;
+
+    // CONTRUCTORS (public)
+    public  Prodotto(String nome, String descrizione, double prezzo, int iva){
+        setCodice();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo= prezzo;
+        this.iva= iva;
+    }
+
+    // GETTERS SETTERS
+    // creo il codice con un numero random a 4 cifre
+    public void setCodice(){
+        Random random = new Random();
+         this.codice = random.nextInt(9000) + 1000 ;
+    }
+
+
+
+    // metto il codice in solo lettura
+    public int getCodice(){
+        return this.codice;
+    }
+
+    // METHODS
+    // metodo per avere un prezzo base
+    public double getPrezzoBase(){
+        return this.prezzo;
+    }
+
+    // metodo per avere un prezzo compreso iva in base alla percentuale  iva inserita
+    public double  getPrezzoIvato(double iva){
+        this.iva = iva;
+        return  this.prezzo + (prezzo/100 * iva);
+    }
+
+    // metodo per avere il nome esteso con codice-nome
+    public String nomeEsteso(){
+        return this.codice + "-"+ this.nome;
+    }
+
+}
---
+++ src/org/lessons/java/shop/main.java
@@ -0,0 +1,42 @@
+package org.lessons.java.shop;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        // Chiedo nome prodotto
+        System.out.println("Nome prodotto: ");
+        String nome = scan.nextLine();
+
+        // Chiedo descrizione prodotto
+        System.out.println("Descrizione prodotto: ");
+        String descrizione = scan.nextLine();
+
+        // Chiedo prezzo prodotto
+        System.out.println("Prezzo prodotto: ");
+        int prezzo= scan.nextInt();
+
+        // Chiedo percentuale iva
+        System.out.println("Percentuale IVA: ");
+        int iva= scan.nextInt();
+
+        // Con i dati costruisco il prodotto
+
+        Prodotto prodotto = new Prodotto( nome, descrizione, prezzo, iva);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        System.out.println("Codice: " + prodotto.getCodice());
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrizione: " + descrizione);
+        System.out.println("Prezzo: " + decimalFormat.format( prodotto.getPrezzoBase()) + " €");
+        System.out.println("Prezzo + iva al " +iva+ "%: " +decimalFormat.format( prodotto.getPrezzoIvato(iva)) + " €");
+        System.out.println("Nome esteso del prodotto: " +prodotto.nomeEsteso());
+
+
+
+        scan.close();
+    }
+}
